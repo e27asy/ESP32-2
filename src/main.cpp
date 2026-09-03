4e321461e29c07e2b73606f6fbafb3d0948943a9
e27asy <e27asy@gmail.com>
2026-09-03 09:33:34 +0700

    tft3.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,101 @@
 #include <Arduino.h>
 
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
 
+// ==========================
+// ESP32
+// ==========================
+#define TFT_CS    15
+#define TFT_RST    4
+#define TFT_DC     2
 
-void setup() {
+#define TFT_MOSI  23
+#define TFT_MISO  19
+#define TFT_SCK   18
 
+Adafruit_ILI9341 tft(
+  TFT_CS,
+  TFT_DC,
+  TFT_RST
+);
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // ESP32 VSPI
+  SPI.begin(
+    TFT_SCK,
+    TFT_MISO,
+    TFT_MOSI,
+    TFT_CS
+  );
+
+  // เริ่ม TFT
+  tft.begin();
+
+  // แนวนอน
+  tft.setRotation(3);
+
+  // พื้นหลัง
+  tft.fillScreen(ILI9341_BLACK);
+
+  // --------------------------
+  // TEST 1
+  // --------------------------
+
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextSize(3);
+
+  tft.setCursor(20, 20);
+  tft.println("ESP32 + ILI9341");
+
+  tft.setCursor(20, 50);
+  tft.println("3.2 TFT SPI");
+
+  tft.drawLine(
+    10, 80,
+    310, 80,
+    ILI9341_RED
+  );
+
+  // --------------------------
+  // TEST COLORS
+  // --------------------------
+
+  tft.fillRect(
+    20, 100,
+    80, 60,
+    ILI9341_RED
+  );
+
+  tft.fillRect(
+    120, 100,
+    80, 60,
+    ILI9341_GREEN
+  );
+
+  tft.fillRect(
+    220, 100,
+    80, 60,
+    ILI9341_BLUE
+  );
+
+  // --------------------------
+  // TEST CIRCLE
+  // --------------------------
+
+  tft.drawCircle(
+    160, 210,
+    40,
+    ILI9341_YELLOW
+  );
+
+  Serial.println("TFT Test Complete");
 }
 
-void loop() {
-  
-  
+void loop()
+{
 }
-
